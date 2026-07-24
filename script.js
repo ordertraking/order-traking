e461df6286608ba89af260bb5f59ab60a79a8235
ordertraking <m.ibrahimsajid830@gmail.com>
2026-07-24 06:01:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,50 +2,44 @@ function trackOrder() {
 
     let order = document.getElementById("orderNo").value.trim();
 
-    if(order==""){
-        document.getElementById("result").innerHTML="<p style='color:red;'>Please enter Order Number.</p>";
+    if (order === "") {
+        document.getElementById("result").innerHTML =
+            "<p style='color:red;'>Please enter Order Number.</p>";
         return;
     }
 
-    document.getElementById("result").innerHTML="Loading...";
+    document.getElementById("result").innerHTML = "<p>Loading...</p>";
 
-    fetch("YOUR_GOOGLE_SCRIPT_URL?order="+encodeURIComponent(order))
+    fetch("https://script.google.com/macros/s/AKfycbwAnKhXTJqXLprnoEvaTByiaPiVKolTab1jKZ3niHpYS1wZeoiOE4cTuyEz3mki87CV/exec?order=" + encodeURIComponent(order))
 
-    .then(response=>response.json())
+    .then(response => response.json())
 
-    .then(data=>{
+    .then(data => {
 
-        if(data.success===false){
+        if (data.success === false || data.message === "Order Not Found") {
 
-            document.getElementById("result").innerHTML="<p style='color:red;'>Order Not Found</p>";
+            document.getElementById("result").innerHTML =
+                "<p style='color:red;'>Order Not Found</p>";
 
-        }else{
+        } else {
 
-            document.getElementById("result").innerHTML=`
-
-            <div style="background:#f5f5f5;padding:15px;border-radius:10px">
-
-            <p><b>Order:</b> ${data.order}</p>
-
-            <p><b>Customer:</b> ${data.customer}</p>
-
-            <p><b>Status:</b> ${data.status}</p>
-
-            <p><b>Date:</b> ${data.date}</p>
-
-            <p><b>Remarks:</b> ${data.remarks}</p>
-
-            </div>
-
+            document.getElementById("result").innerHTML = `
+                <div style="background:#f5f5f5;padding:15px;border-radius:10px;">
+                    <p><b>Order:</b> ${data.order}</p>
+                    <p><b>Customer:</b> ${data.customer}</p>
+                    <p><b>Status:</b> ${data.status}</p>
+                    <p><b>Date:</b> ${data.date}</p>
+                    <p><b>Remarks:</b> ${data.remarks}</p>
+                </div>
             `;
-
         }
 
     })
 
-    .catch(error=>{
+    .catch(error => {
 
-        document.getElementById("result").innerHTML="<p style='color:red;'>Connection Error</p>";
+        document.getElementById("result").innerHTML =
+            "<p style='color:red;'>Connection Error</p>";
 
         console.log(error);
 
